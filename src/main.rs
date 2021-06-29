ab2a9640909d8aa130c36718057b103bb15cdaac
Cixtor <cixtords@gmail.com>
2021-06-29 10:24:38 -0800

    Add unimplemented functions to mark basic program features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env::args;
+
 fn main() {
-    println!("Hello, world!");
+    let command = args().nth(1);
+
+    match command.as_ref().map(String::as_ref) {
+        Some("list") => list(),
+        Some("recent") => recent(),
+        Some("help") => help(),
+        None => help(),
+        _ => create(),
+    }
+}
+
+fn list() {
+    unimplemented!();
+}
+
+fn recent() {
+    unimplemented!();
+}
+
+fn help() {
+    unimplemented!();
+}
+
+fn create() {
+    unimplemented!();
 }
