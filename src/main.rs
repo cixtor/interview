89dc7a33ad9492f4bc3985347547d3da72dd73c0
Cixtor <cixtords@gmail.com>
2021-06-30 09:00:00 -0800

    Add function to recursively find all files in a directory

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 use std::env::args;
+use std::fs;
+use std::path::PathBuf;
+
+#[derive(Debug)]
+pub enum MyErrors {
+    CannotReadDirectory,
+}
 
 fn main() {
     let command = args().nth(1);
@@ -14,6 +21,30 @@ fn main() {
 
 fn list() {
     unimplemented!();
+}
+
+fn list_files(dir: PathBuf) -> Result<Vec<PathBuf>, MyErrors> {
+    let mut all_files = Vec::new();
+
+    if let Ok(entries) = fs::read_dir(dir) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                if let Ok(meta) = entry.metadata() {
+                    if meta.is_dir() {
+                        if let Ok(mut res) = list_files(entry.path()) {
+                            all_files.append(&mut res);
+                        }
+                    } else {
+                        all_files.push(entry.path());
+                    }
+                }
+            }
+        }
+    } else {
+        return Err(MyErrors::CannotReadDirectory);
+    }
+
+    return Ok(all_files);
 }
 
 fn recent() {
